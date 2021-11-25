12459c6446cfd1a9986ea25a006f08181f172709
Jhonny Flores <88248110+JhonnyDark10@users.noreply.github.com>
2021-11-24 20:14:09 -0500

    update css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import "./styles.css";
+import React from "react";
+import { Theme } from "./Theme";
+import { Title } from "./Title";
 import Form from "./Form";
+
 const movies = [
   {
     name: "Avengers",
     available: 5
   },
   {
-    name: "marvel",
+    name: "Terminator",
     available: 3
   }
 ];
+
 export default function App() {
+  const [theme, setTheme] = React.useState("avengers");
+
   return (
-    <div>
-      <h2>Peliculas</h2>
+    <Theme theme={theme}>
+      <Title>Películas</Title>
       {movies.map((movie) => (
-        <Form movie={movie} />
+        <Form movie={movie} updateTheme={() => setTheme(movie.name)} />
       ))}
-    </div>
+    </Theme>
   );
 }
